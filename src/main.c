ae099ff74c3e367b13f16c0df4efb93cc1c396a0
Nick Miller <nick.miller.83@gmail.com>
2021-06-24 11:48:59 -0700

    hex follows mouse

--- src/main.c
+++ src/main.c
@@ -14,6 +14,11 @@
 static SDL_Window* g_window = NULL;
 static SDL_Renderer* g_renderer = NULL;
 static SDL_Texture* g_texture = NULL;
+static bool g_follow_mouse = true;
+static int g_mouse_x = 0;
+static int g_mouse_y = 0;
+static int g_hex_x = 0;
+static int g_hex_y = 0;
 static bool g_game_running = true;
 
 //-----------------------------------------------------------------------------
@@ -110,29 +115,49 @@ static void process_input(void) {
         if (e.type == SDL_KEYDOWN) {
             g_game_running = false;
         }
-        if (e.type == SDL_MOUSEBUTTONDOWN) {
-            g_game_running = false;
+        if (e.type == SDL_MOUSEBUTTONUP) {
+            g_follow_mouse = !g_follow_mouse;
+        }
+        if (e.type == SDL_MOUSEMOTION) {
+            SDL_GetMouseState(&g_mouse_x, &g_mouse_y);
         }
     }
 }
 
 static void update(void) {
+    if (g_follow_mouse) {
+        g_hex_x = g_mouse_x;
+        g_hex_y = g_mouse_y;
+    }
 }
 
 // Render the texture without stretching at (x,y).
-static void render_texture_at(SDL_Texture* texture, int x, int y) {
+static void render_texture_at(SDL_Texture* texture, int x, int y, bool centered) {
     int src_w = 0;
     int src_h = 0;
     SDL_QueryTexture(texture, NULL, NULL, &src_w, &src_h);
 
-    SDL_Rect dest = { .x = x, .y = y, .w = src_w, .h = src_h };
+    SDL_Rect dest = {
+        .x = x,
+        .y = y,
+        .w = src_w,
+        .h = src_h
+    };
+    if (centered) {
+        dest.x -= src_w / 2;
+        dest.y -= src_h / 2;
+    }
     SDL_RenderCopy(g_renderer, texture, NULL, &dest);
+}
+
+static void render_texture_centered_at(SDL_Texture* texture, int x, int y) {
+    render_texture_at(texture, x, y, true);
 }
 
 static void render(void) {
     SDL_RenderClear(g_renderer);
     SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    render_texture_at(g_texture, WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
+    render_texture_centered_at(g_texture, g_hex_x, g_hex_y);
     SDL_RenderPresent(g_renderer);
 }
 
